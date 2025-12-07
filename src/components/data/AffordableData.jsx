489913243b7b1826d0c0a66dcd4b8911f016abd7
ShrouqElsayed123 <shrouqwork123@gmail.com>
2025-12-07 11:17:48 +0200

    update ....

--- src/components/data/AffordableData.jsx
+++ src/components/data/AffordableData.jsx
@@ -28,35 +28,35 @@ export const AffordableData =
                 },
                 {
                     text: "100% Renewable Energy Pledge",
-                    pdf: "/mnu/pdfs/Activities/SDG17/100% Renewable Energy Pledge.pdf"
+                    pdf: "/mnu/pdfs/Activities/SDG7/100 Renewable Energy Pledge.pdf"
                 },
                 {
                     text: "University undergo energy reviews to identify areas where energy wastage is highest",
-                    pdf: "/mnu/pdfs/Activities/SDG17/University undergo energy reviews to identify areas where energy wastage is highest.pdf"
+                    pdf: "/mnu/pdfs/Activities/SDG7/University undergo energy reviews to identify areas where energy wastage is highest.pdf"
                 },
                 {
                     text: "Plans to upgrade existing buildings tohigher energy efficiency",
-                    pdf: "/mnu/pdfs/Activities/SDG17/Plans to upgrade existing buildings tohigher energy efficiency.pdf"
+                    pdf: "/mnu/pdfs/Activities/SDG7/Plans to upgrade existing buildings tohigher energy efficiency.pdf"
                 },
                 {
                     text: "Plan to reduce energy consumption",
-                    pdf: "/mnu/pdfs/Activities/SDG17/Plan to reduce energy consumption.pdf"
+                    pdf: "/mnu/pdfs/Activities/SDG7/Plan to reduce energy consumption.pdf"
                 },
                 {
                     text: "Low carbon energy use",
-                    pdf: "/mnu/pdfs/Activities/SDG17/Low carbon energy use.pdf"
+                    pdf: "/mnu/pdfs/Activities/SDG7/Low carbon energy use.pdf"
                 },
                 {
                     text: "Energy usage per sqm",
-                    pdf: "/mnu/pdfs/Activities/SDG17/Energy usage per sqm.pdf"
+                    pdf: "/mnu/pdfs/Activities/SDG7/Energy usage per sqm.pdf"
                 },
                 {
                     text: "Energy efficiency services for industry",
-                    pdf: "/mnu/pdfs/Activities/SDG17/Energy efficiency services for industry.pdf"
+                    pdf: "/mnu/pdfs/Activities/SDG7/Energy efficiency services for industry.pdf"
                 },
                 {
                     text: "Carbon Reduction & Emission Reduction Process",
-                    pdf: "/mnu/pdfs/Activities/SDG17/Carbon Reduction & Emission Reduction Process.pdf"
+                    pdf: "/mnu/pdfs/Activities/SDG7/Carbon Reduction & Emission Reduction Process.pdf"
                 },
 
             ],
@@ -64,11 +64,11 @@ export const AffordableData =
         {
             id: "3",
             label: "Partnerships",
-            title: "",
+            title: "Partnerships",
             content: [
                 {
                     text: "Local Community Outreach for Energy Efficiency",
-                    link: "/mnu//pdfs/Partnerships/SDG7Local Community Outreach for Energy Efficiency.pdf"
+                    link: "/mnu//pdfs/Partnerships/SDG7/Local Community Outreach for Energy Efficiency.pdf"
                 }
                 // {
                 //     text: "",
